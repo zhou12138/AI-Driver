2f7b093d6db78a6995c365b061eaabf87df64733
zhouweiwei <zhouweiwei@microsoft.com>
2026-05-26 17:49:25 +0800

    chore: remove temp script

--- _push.mjs
+++
@@ -1,5 +0,0 @@
-import { execSync } from 'child_process';
-process.chdir('c:\\edge_workspace\\drive\\street-view-app');
-execSync('git add -A', { stdio: 'inherit' });
-execSync('git commit -m "docs: detail AI hybrid navigation architecture"', { stdio: 'inherit' });
-execSync('git push', { stdio: 'inherit' });
